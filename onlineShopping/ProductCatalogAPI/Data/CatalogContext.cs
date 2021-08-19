88c6ebf1f61386a579350ff7e620f7251afc3bf2
mike4741 <64281489+mike4741@users.noreply.github.com>
2021-08-19 08:54:27 -0500

    Initial commit

---
+++ onlineShopping/ProductCatalogAPI/Data/CatalogContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductCatalogAPI.Data
+{
+    public class CatalogContext : DbContext
+    {
+        public CatalogContext(DbContextOptions options ): base(options)
+        {
+
+        }
+        public DbSet<CatalogType> CatalogTypes { get; set; }
+        public DbSet<CatalogBrand> CatalogBrands { get; set; }
+        public DbSet<CatalogItem> CatalogItems { get; set; }
+    }
+}
